93007294ec8ca080abd50756c91c3251b8165417
AmandaSoganile <soganileamanda@gmail.com>
2026-01-28 14:31:07 +0200

    Initial Commit

---
+++ HustleConnect/HustleConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HustleConnectApp.swift
+//  HustleConnect
+//
+//  Created by Amanda Soganile on 28/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct HustleConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
